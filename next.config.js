a55ea9bc9df652f1a3ea420dfe8cda537cd7fbf1
Vila Juan Cruz <yunagosh7@gmail.com>
2023-08-17 23:58:39 -0300

    Hice el header

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // Allows import images from domains specified
+    // images: {
+    //     domains:['domain.com']
+    // }
+}
 
 module.exports = nextConfig
