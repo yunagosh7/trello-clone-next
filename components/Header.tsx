7ea5172ec2b43ce8a570cbb2603d5121603c4255
Vila Juan Cruz <yunagosh7@gmail.com>
2023-08-21 23:35:27 -0300

    Añadi el context y las funciones para hacer el crud en el context

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,6 +10,11 @@ function Header() {
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+        
+        <div 
+          className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055d1] rounded-md filter blur-3xl opacity-50 -z-50"
+        />
+        
         <Image
           src={logo}
           alt="Trello logo"
@@ -37,8 +42,8 @@ function Header() {
         </div>
       </div>
 
-    <div className="flex items-center justify-center px-5 md:py-5">
-        <p className="flex items-center text-sm font-light p-5 pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055d1]">
+    <div className="flex items-center justify-center px-5 py-2 md:py-5">
+        <p className="flex items-center text-sm font-light p-5 pr-5  shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055d1]">
             <UserCircleIcon className="inline-block h-10 w-10 text-[#0055d1] mr-1" />
             GPT is summarising your tasks for the day...
         </p>
