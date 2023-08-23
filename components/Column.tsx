f73d76fc0c5049b198c547db475180916b2258fe
Vila Juan Cruz <yunagosh7@gmail.com>
2023-08-23 10:58:43 -0300

    Añadi los tipos y los componentes de TodoCard y Column

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,4 +1,6 @@
 import { Todo, TypedColumn } from "@/typings";
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
+import { todo } from "node:test";
 import React from "react";
 import { Draggable, Droppable } from "react-beautiful-dnd";
 
@@ -6,6 +8,14 @@ type Props = {
   id: TypedColumn;
   todos: Todo[];
   index: number;
+};
+
+const idToColumnText: {
+  [key in TypedColumn]: string;
+} = {
+  todo: "To do",
+  inprogress: "In progress",
+  done: "Done",
 };
 
 function Column({ id, todos, index }: Props) {
@@ -27,7 +37,39 @@ function Column({ id, todos, index }: Props) {
                   snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"
                 }`}
               >
-                <h2>{id}</h2>
+                <h2 className="flex justify-between font-bold text-lg p-2">
+                  {idToColumnText[id]}
+
+                  <span className="text-gray-500 bg-gray-200 rounded-full font-normal px-2 py-2 text-sm font-normal">
+                    {todos.length}
+                  </span>
+                </h2>
+                <div className="space-y-2">
+                  {todos.map((todo, index) => (
+                    <Draggable
+                      key={todo.$id}
+                      draggableId={todo.$id}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <TodoCard
+                          todo={todo}
+                          index={index}
+                          id={id}
+                          innerRef={provided.innerRef}
+                          draggableProps={provided.draggableProps}
+                          dragHandleProps={provided.dragHandleProps}
+                        />
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                  <div className="flex items-center justify-end">
+                    <button className="text-gray-500 hover:text-green-600">
+                      <PlusCircleIcon className="h-10 w-10 " />
+                    </button>
+                  </div>
+                </div>
               </div>
             )}
           </Droppable>
---
+++ components/TodoCard.tsx
@@ -0,0 +1,21 @@
+import { Todo, TypedColumn } from "@/typings";
+import React from "react";
+import {
+  DraggableProvidedDragHandleProps,
+  DraggableProvidedDraggableProps,
+} from "react-beautiful-dnd";
+
+type TodoCardProps = {
+  todo: Todo;
+  index: number;
+  id: TypedColumn;
+  innerRef: (element: HTMLElement | null) => void;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps: DraggableProvidedDragHandleProps | null | undefined;
+};
+
+function TodoCard() {
+  return <div>TodoCard</div>;
+}
+
+export default TodoCard;
